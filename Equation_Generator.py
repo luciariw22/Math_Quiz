800caf27db4ec0baf88a1b34c5a488a6cdea90ed
luciariw22 <125606190+luciariw22@users.noreply.github.com>
2023-06-09 09:12:38 +1200

    Add files via upload

--- Equation_Generator.py
+++ Equation_Generator.py
@@ -3,18 +3,29 @@ import random
 low_number = 1
 high_number = 20
 
-num1 = random.randint(low_number, high_number)
-num2 = random.randint(low_number, high_number)
+number = random.randint(low_number, high_number)
+# gives x's value high/low number
+x = random.choice(['low', 'high'])
 
-print(num1, "+", num2, "=")
+if x == 'low':
+    x_value = low_number
+else:
+    x_value = high_number
+
+print(f"x = {x_value}")
+print(f"{number} + x =")
 
 user_input = input("Answer: ")
 
 # Convert the user input to an integer
 user_result = int(user_input)
 
+# Get the correct answer
+correct_answer = number + x_value
+
 # Compare the user's input with the correct answer
-if user_result == num1 + num2:
+if user_result == correct_answer:
     print("Correct!")
 else:
-    print("Incorrect. The answer is", num1 + num2)
+    print(f"Incorrect. The answer is {correct_answer}")
+
